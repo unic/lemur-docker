4f568ef119f7d8cabf7ebd76d4f02591921d3f8b
Mathias Petermann <mathias.petermann@projektfokus.ch>
2020-11-06 14:02:41 +0100

    Improve celery beat/worker scripts

--- celery/beat-start.sh
+++ celery/beat-start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-CURRENT_USER=$(id -u)
-CURRENT_GROUP=$(id -g)
-
-echo "Starting celery beat with user $CURRENT_USER and group $CURRENT_GROUP"
-echo "Configured log_level: ${CELERY_LOG_LEVEL:-info}"
+echo "Starting celery beat with user $(whoami)($(id -u)) and group $(groups celery | awk -F":" '{print $2}' | awk '{print $1}')($(id -g))"
+echo -e "Configured log_level: ${CELERY_LOG_LEVEL:-info}\n\n"
 
 /usr/local/src/lemur/venv/bin/celery -A lemur.common.celery beat --loglevel=${CELERY_LOG_LEVEL:-info}
