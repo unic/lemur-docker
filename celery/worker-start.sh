c6ee49a4ff6990a965b461329408624d6b3336fd
Mathias Petermann <mathias.petermann@projektfokus.ch>
2020-11-06 13:52:58 +0100

    Add celery user, Improve celery start scripts, to print user & log level, and make log level configurable

--- celery/worker-start.sh
+++ celery/worker-start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-/usr/local/src/lemur/venv/bin/celery -A lemur.common.celery worker --concurrency 10 -E -n lemurworker1@%%h --loglevel=info
+CURRENT_USER=$(id -u)
+CURRENT_GROUP=$(id -g)
+
+echo "Starting celery beat with user $CURRENT_USER and group $CURRENT_GROUP"
+echo "Configured log_level: ${CELERY_LOG_LEVEL:-info}"
+
+/usr/local/src/lemur/venv/bin/celery -A lemur.common.celery worker --concurrency 10 -E -n lemurworker1@%%h --loglevel=${CELERY_LOG_LEVEL:-info}
